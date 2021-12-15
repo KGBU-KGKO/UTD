b8e74370bab11119f5d431751266706cca507b27
Stryukov <stryukov.semen@gmail.com>
2021-12-15 17:49:24 +1200

    Add filling form
    added filling forms for legal entities, government bodies and a number of improvements

--- js/utd.js
+++ js/utd.js
@@ -1,10 +1,52 @@
 $.when( $.ready ).then(function() {
-  console.log('123');
+  $('#numSMEV').prop('disabled', "true");
+  $('#reqNum').val('02-20/2628');
+
+  //   console.log(new Date());
+  // $('#reqDate').val($.now());
+
+
 });
 
-$('#agentSwitch').on('switchChange.bootstrapSwitch', function (event, state) {
-  console.log(state);
-}); 
+
+$('#agentFLSwitch').change(function() {
+  if ($(this).prop('checked')) {
+    $('#agentFLForm').show();
+  } else {
+    $('#agentFLForm').hide();
+  }
+})
+
+$('#declarantType').on('change', function() {
+  $('.declarant').hide();
+  $('#numSMEV').prop('disabled', "true");
+  switch (this.value) {
+    case 'ФЛ':
+      $('#declarantFL').show();
+      break;
+    case 'ЮЛ':
+      $('#declarantUL').show();
+      break;
+    case 'ОГВ':
+      $('#declarantOGV').show();
+      $('#numSMEV').prop('disabled', "");
+      break;  }
+});
+
+$( ".attach button" ).click(function() {
+  if (!$('#attachList').val()) {
+    $('#attachList').val($(this).text());
+  } else {
+    let attachList = $('#attachList').val().split(', ');
+    if($.inArray($(this).text(), attachList) == -1) {
+      $('#attachList').val($('#attachList').val()+', '+$(this).text());      
+    }
+  }
+});
+
+$( "#send" ).click(function() {
+
+});
 
 // $.when(
 //   $.getJSON( "ajax/test.json" ),
